7111e64db3cc85c4d36ca14b2a0e5d5e4701792e
Anntvanntv <108679162+anntvanntv@users.noreply.github.com>
2024-10-18 22:53:43 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -53,3 +53,28 @@ figuras.forEach(function(figura){
 
 
 
+
+//COOKIES
+
+let cookieModal = document.querySelector(".cookie-consent-modal")
+let cancelCookieBtn = document.querySelector(".cookies-btn.cancel")
+let acceptCookieBtn = document.querySelector(".cookies-btn.accept")
+
+
+cancelCookieBtn.addEventListener("click", function(){
+    cookieModal.classList.remove("active")
+})
+
+acceptCookieBtn.addEventListener("click", function(){
+    cookieModal.classList.remove("active")
+    localStorage.setItem("cookieAccepted", "yes")
+})
+
+setTimeout(function (){
+    let cookieAccepted = localStorage.getItem("cookieAccepted")
+    if (cookieAccepted != "yes"){
+        cookieModal.classList.add("active")
+    }
+}, 2000)
+
+
